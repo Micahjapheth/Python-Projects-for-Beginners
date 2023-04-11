183bc39d177b3228ef429ee0b3d31c2e5c7f7c08
Micahjapheth <79207314+Micahjapheth@users.noreply.github.com>
2023-04-11 22:12:59 +0300

    Update Email Slicer.py

--- Email Slicer/Email Slicer.py
+++ Email Slicer/Email Slicer.py
@@ -1,6 +1,6 @@
-email = input("Enter Your Email: ").strip()
+email = input("micahjapheth@gmail.com: ").strip()
 
-username = email[:email.index('@')]
-domain = email[email.index('@') + 1:]
+micahjapheth = micahjapheth@gmail.com[:micahjapheth@gmail.index('@')]
+gmail.com = jmarymwende@gmail.com[jmarymwende@gmail.index('@') + 1:]
 
-print(f"Your username is {username} & domain is {domain}")
+print(f"Your username is {micahjapheth} & domain is {gmail.index}")
